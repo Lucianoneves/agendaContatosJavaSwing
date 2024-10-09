54035f2efa48cfe2d74c8a66b5c6456ddef9cbfc
luciano <lucianogudas057@gmail.com>
2024-10-08 22:34:30 -0300

    Agenda de contatos em Java Swing

---
+++ src/main/java/com/mycompany/aendadecontatos/AendaDeContatos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aendadecontatos;
+
+/**
+ *
+ * @author Administrador
+ */
+public class AendaDeContatos {
+
+    public static void main(String[] args) {
+        Tela form = new Tela();
+        form.setVisible(true);
+        
+    }
+}
